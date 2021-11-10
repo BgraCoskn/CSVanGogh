155d4436a687d0975a78aa43226433e384e4acbb
bcskn <bcskn@pop-os.localdomain>
2021-11-10 20:17:57 +0300

    Changed branch to practical, this is the branch where the script is mostly manual instead of automatic.

--- mancsv.py
+++ mancsv.py
@@ -21,7 +21,7 @@ class csvf:
     #CSV file class,
     #inputs = string type, path to CSV file
     def __init__(self, filepath):
-        headpad = 30; footpad = 20
+        headpad = 1; footpad = 20
         self.name = os.path.basename(filepath)
         self.data = pd.read_csv(filepath,\
                 engine='python', skiprows=headpad, skipfooter=footpad)
@@ -48,6 +48,7 @@ class csvf:
                 colnames.append(colname)
             self.colnames = colnames
             self.data = self.data.set_axis(colnames, axis=1)
+            print(colnames)
             print(self.data)
 
 class pltcls:
@@ -96,7 +97,7 @@ class pltcls:
 
     def update(self,
             fig_title, fig_xlabel, fig_ylabel, fig_ylabel2=None, ):
-        
+
         self.fig.update_layout(title_text=fig_title)
         self.fig.update_xaxes(title_text=fig_xlabel)
         self.fig.update_yaxes(title_text=fig_ylabel, secondary_y=False)
@@ -120,16 +121,89 @@ def main(csv_file, ch_cols, figlabels):
     csv_data= csvf(csv_file)
     print(csv_data.colnames)
     theplot = pltcls(csv_data.data, ch_cols)
-    theplot.update(figlabels[0], figlabels[1], figlabels[2], figlabels[3])
+    #theplot.update(figlabels[0], figlabels[1], figlabels[2], figlabels[3])
     # Its show time
     theplot._show_()
-    pio.write_html(theplot.fig, file='index.html', auto_open=True)
-    with socketserver.TCPServer(("", PORT), Handler) as httpd:
-        print("Plot serving at", PORT)
-        httpd.serve_forever()
-# Example
-main("Test1_Kuzey_Platform_Esneme.csv",
-    ["Motorposx","Motorposy", "XactualDegree", "YactualDegree"],
-    ["Motor pozisyonu ve Inklinometre açısı",
-        "Zaman(s)","MotorPoz", "InklinometreDeg"])
+    pio.write_html(theplot.fig, file=theplot.name+'.html', auto_open=True)
 
+# Example
+#main("Test1_Kuzey_Platform_Esneme.csv",
+#  ["Motorposx","Motorposy", "XactualDegree", "YactualDegree"],
+#   ["Motor pozisyonu ve Inklinometre açısı",
+#       "Zaman(s)","MotorPoz", "InklinometreDeg"])
+
+"""
+main("lasertracker.csv",
+    ["A"],
+    [".",
+        "Zaman(s)","."])
+main("lastrack2.csv",["LposfeedY"],[".",".","."])
+""""""
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Plot serving at", PORT)
+    httpd.serve_forever()
+
+""" 
+
+def get_data(csvobj, colname):
+    datalst = csvobj.data[colname].tolist()
+    return datalst
+
+def get_error(dt1, dt2):
+    # Match the data list lengths
+    if len(dt1) >= len(dt2): dt1 = dt1[:len(dt2)]
+    elif len(dt2) >= len(dt1): dt2 = dt2[:len(dt1)]
+
+    errlst = [0 for x in range(len(dt1))] # Define the error dataset
+
+    # Calculate the difference of datasets
+    cnt = 0
+    for d1, d2 in zip(dt1, dt2):
+        errlst[cnt] = (d1-d2)
+        cnt += 1
+
+    return errlst
+
+timeser = []
+def adtrc(figobj,datalst,strname,secondy=False):
+    # Add a graph to the figure
+
+    timeser = [*range(0,len(datalst))]  # Timeseries is the data number
+    timeser = [x/100 for x in timeser] # SamplingPeriod=10ms
+
+    figobj.add_trace(go.Scatter(x=timeser, y=datalst, name=strname),
+            secondary_y=secondy)
+
+# -----------------------------------------------------------------------------
+
+# CSV Files to be read, get the column names from the outputs
+csvd1 = csvf("lasertracker22.csv")
+csvd2 = csvf("test2_son.csv")
+
+# Data lists
+lposfeedy = get_data(csvd1, "LposfeedY")
+lasery = get_data(csvd2, "LaserY")
+lposfeedyext = get_data(csvd1, "LposfeedYext")
+lposrefy = get_data(csvd1, "LposrefY")
+
+# Error lists
+las_inc =get_error(lasery, lposfeedy)
+las_ext = get_error(lasery, lposfeedyext)
+las_ref = get_error(lasery, lposrefy)
+ref_inc = get_error(lposrefy, lposfeedy)
+
+fig = make_subplots(specs=[[{"secondary_y":True}]]) # Add a second Y-axis
+fig.update_layout(plot_bgcolor="#eee") # Background Color
+
+adtrc(fig, lposfeedy, "lposfeedY", secondy=False)
+adtrc(fig, lasery, "laserY", secondy=False)
+adtrc(fig, lposfeedyext, "lposFeedYext", secondy=False)
+adtrc(fig, lposrefy, "lPosRefY", secondy=False)
+
+adtrc(fig, las_inc, "LaserY-lPosFeedY", secondy=True)
+adtrc(fig, las_ext, "LaserY-lPosFeedYext", secondy=True)
+adtrc(fig, las_ref, "LaserY-lPosRefY", secondy=True)
+adtrc(fig, ref_inc, "lPosRefY-lposFeedY", secondy=True)
+
+fig.show()
+pio.write_html(fig, file='LaserTrackerError.html', auto_open=True)
